b50edcab1bda2e81cbb90398d47f143b2b30e9a7
lassel <larpatin@gmail.com>
2020-12-07 21:14:53 +0200

    at, castarray, chunk, clamp tests added

--- unit_tests/drop.test.js
+++ unit_tests/drop.test.js
@@ -3,18 +3,18 @@ import drop from '../src/drop'
 const numbers = [1,2,3,4,5];
 const defaultResult = [2,3,4,5];
 
-test('test drop with negative index', () => {
+test('test drop with negative element count', () => {
     expect(drop(numbers, -1)).toStrictEqual(numbers);
 })
 
-test('test drop with outside array index', () => {
+test('test drop with more elements than in array', () => {
     expect(drop(numbers, 100000)).toStrictEqual([]);
 })
 
-test('test drop with null index', () => {
+test('test drop with null amount elements', () => {
     expect(drop(numbers, null)).toStrictEqual(defaultResult);
 })
 
-test('test drop with outside array index', () => {
+test('test drop with undefined amount of elements', () => {
     expect(drop(numbers, undefined)).toStrictEqual(defaultResult);
 })
